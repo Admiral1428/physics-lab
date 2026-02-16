831d8a9cd519778e52d358a43da27bbb180ace7f
Tim Roble <tradmiral14@gmail.com>
2026-02-16 15:17:44 -0500

    Implemented impulse-based particle collisions

--- src/core/constants.hpp
+++ src/core/constants.hpp
@@ -45,16 +45,33 @@ namespace Constants
 
 
     // Target time interval for how often to update physics
-    inline constexpr float PHYS_DT = 1.0f / 120.0f;
+    inline constexpr float PHYS_DT = 1.0f / 240.0f;
 
     // Max number of physics updates per rendering frame (prevent "spiral of death")
     inline constexpr int MAX_PHYS_STEPS = 5;
 
     // Default attributes for particle
-    inline constexpr float PARTICLE_MASS = 0.1;
+    inline constexpr float PARTICLE_MASS = 0.1f;
     inline constexpr float PARTICLE_RADIUS = 6.0f/(2*PIXELS_PER_METER);  // equivalent to 6 pixel diameter
-    inline constexpr float PARTICLE_MU = 0.1;  // static and dynamic friction coefficient
-    inline constexpr float PARTICLE_RESTITUTION = 0.5;  // 0 no bounce, 1 fully elastic (no energy loss)
+    inline constexpr float PARTICLE_MU = 0.1f;  // static and dynamic friction coefficient
+    inline constexpr float PARTICLE_RESTITUTION = 0.5f;  // 0 no bounce, 1 fully elastic (no energy loss)
+
+    // Grid for particle collision detection
+    inline constexpr int CELL_SIZE = PARTICLE_RADIUS*2;
+    inline constexpr int OFFSETS[9][2] = {
+        { 0, 0}, { 1, 0}, {-1, 0},
+        { 0, 1}, { 0,-1},
+        { 1, 1}, { 1,-1}, {-1, 1}, {-1,-1}
+    };
+
+    // Solver iterations to help with stability of collision detection
+    inline constexpr int SOLVER_ITERS = 20;
+
+    // Constants for positional correction to prevent jitter
+    inline constexpr float SLOP = 0.002f;
+    inline constexpr float PCT = 0.3f;
+    inline constexpr float CLAMP_PEN = 0.05f;
+    inline constexpr float CLAMP_CORR = 0.02f;
 
     // Default gravity in meters per second squared
     inline constexpr float GRAV = -9.81f;
--- src/physics/PhysicsSystem.hpp
+++ src/physics/PhysicsSystem.hpp
@@ -18,9 +18,15 @@ public:
 private:
     void applyGravity();
     void checkScreenCollisions();
+    size_t hashCell(int x, int y);
+    void addParticlesToGrid();
+    void checkParticleCollisions();
+    bool checkCollision(const Particle& A, const Particle& B);
+    void resolveCollision(Particle& A, Particle& B);
 
     std::vector<Particle> particles;
     std::unique_ptr<Integrator> integrator;
+    std::unordered_map<size_t, std::vector<Particle*>> grid;
 };
 
 #endif
